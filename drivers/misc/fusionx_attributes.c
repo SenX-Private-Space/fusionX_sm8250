363bc86fdf563028e08ddeeb57fdcd3aa1a06846
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-01-07 10:34:57 +0700

    fusionx_attr: allow to change different treatment to dirty pte
    Rebranded from e404_attr.
    
    "arm64, mm: avoid always making PTE dirty in pte_mkwrite()" needed.
    
    Co-authored-by: bimoalfarrabi <bimoalfarrabi24@gmail.com>
    Co-authored-by: SenseiiX <senseixdev@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/misc/fusionx_attributes.c
+++ drivers/misc/fusionx_attributes.c
@@ -6,53 +6,59 @@
 #include <linux/fusionx_attributes.h>
 
 struct fusionx_attributes fusionx_data = {
-    .kgsl_skip_zeroing = 0,
+	.kgsl_skip_zeroing = 0,
+	.avoid_dirty_pte = 0,
 };
 
-#define FUSIONX_ATTR_RW(name) \
-static ssize_t name##_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) \
-{ \
-    return sprintf(buf, "%d\n", fusionx_data.name); \
-} \
-static ssize_t name##_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count) \
-{ \
-    int val; \
-    if (kstrtoint(buf, 10, &val)) \
-        return -EINVAL; \
-    fusionx_data.name = val; \
-    return count; \
-} \
-static struct kobj_attribute name##_attr = __ATTR(name, 0644, name##_show, name##_store);
+#define FUSIONX_ATTR_RW(name)                                                     \
+	static ssize_t name##_show(struct kobject *kobj,                       \
+				   struct kobj_attribute *attr, char *buf)     \
+	{                                                                      \
+		return sprintf(buf, "%d\n", fusionx_data.name);                   \
+	}                                                                      \
+	static ssize_t name##_store(struct kobject *kobj,                      \
+				    struct kobj_attribute *attr,               \
+				    const char *buf, size_t count)             \
+	{                                                                      \
+		int val;                                                       \
+		if (kstrtoint(buf, 10, &val))                                  \
+			return -EINVAL;                                        \
+		fusionx_data.name = val;                                          \
+		return count;                                                  \
+	}                                                                      \
+	static struct kobj_attribute name##_attr =                             \
+		__ATTR(name, 0644, name##_show, name##_store);
 
 FUSIONX_ATTR_RW(kgsl_skip_zeroing);
+FUSIONX_ATTR_RW(avoid_dirty_pte);
 
-static struct attribute *fusionx_attrs[] = {
-    &kgsl_skip_zeroing_attr.attr,
-    NULL
-};
+static struct attribute *fusionx_attrs[] = { &kgsl_skip_zeroing_attr.attr,
+					  &avoid_dirty_pte_attr.attr, NULL };
 
 static struct attribute_group fusionx_attr_group = {
-    .attrs = fusionx_attrs,
+	.attrs = fusionx_attrs,
 };
 
 static struct kobject *fusionx_kobj;
 
-static int __init fusionx_attributes_init(void) {
-    int retval;
+static int __init fusionx_attributes_init(void)
+{
+	int retval;
 
-    fusionx_kobj = kobject_create_and_add("fusionx_attributes", kernel_kobj);
-    if (!fusionx_kobj)
-        return -ENOMEM;
+	fusionx_kobj = kobject_create_and_add("fusionx_attributes", kernel_kobj);
+	if (!fusionx_kobj)
+		return -ENOMEM;
 
-    retval = sysfs_create_group(fusionx_kobj, &fusionx_attr_group);
-    if (retval)
-        kobject_put(fusionx_kobj);
+	retval = sysfs_create_group(fusionx_kobj, &fusionx_attr_group);
+	if (retval)
+		kobject_put(fusionx_kobj);
 
-    return retval;
+	return retval;
 }
 
-static void __exit fusionx_attributes_exit(void) {
-    kobject_put(fusionx_kobj);
+static void __exit fusionx_attributes_exit(void)
+{
+	kobject_put(fusionx_kobj);
 }
 
 module_init(fusionx_attributes_init);
