9ab0e4ff9a0b905afa4538fc1f7bf0ba0e604eb9
SENX | センクス <senseixdev@gmail.com>
2026-02-26 13:56:03 +0000

    fusionx_attr: introduce background app blocker
    Rebranded from:
    https://github.com/kvsnr113/xiaomi_sm8250_kernel/commit/e48562c41962
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    [viasco: Rebranded e404 to n0kz]
    Signed-off-by: viasco <viasco@n0kontzzz>
    Signed-off-by: SENX | センクス <senseixdev@gmail.com>

--- include/linux/fusionx_attributes.h
+++ include/linux/fusionx_attributes.h
@@ -7,9 +7,17 @@
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
 
+#include <linux/sched.h>
+
+#define FUSIONX_BLOCKLIST_STRLEN 256
+#define FUSIONX_MAX_BLOCKED 16
+
+bool fusionx_comm_blocked(const char *comm);
+
 struct fusionx_attributes {
     int kgsl_skip_zeroing;
     int avoid_dirty_pte;
+    char bg_blocklist[FUSIONX_BLOCKLIST_STRLEN];
 };
 
 extern struct fusionx_attributes fusionx_data;
