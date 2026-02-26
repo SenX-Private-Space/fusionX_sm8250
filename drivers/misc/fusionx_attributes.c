78e98a4edb8f87977d549cee14521dd144044de8
SENX | センクス <senseixdev@gmail.com>
2026-02-26 13:56:03 +0000

    fusionx_attr: introduce background app blocker
    Rebranded from:
    https://github.com/kvsnr113/xiaomi_sm8250_kernel/commit/e48562c41962
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    [viasco: Rebranded e404 to n0kz]
    Signed-off-by: viasco <viasco@n0kontzzz>
    Signed-off-by: SENX | センクス <senseixdev@gmail.com>

--- drivers/misc/fusionx_attributes.c
+++ drivers/misc/fusionx_attributes.c
@@ -5,10 +5,75 @@
 #include <linux/kobject.h>
 #include <linux/fusionx_attributes.h>
 
+static char fusionx_blocked[FUSIONX_MAX_BLOCKED][TASK_COMM_LEN];
+static u8 fusionx_blocked_len[FUSIONX_MAX_BLOCKED];
+static int fusionx_blocked_cnt;
+
 struct fusionx_attributes fusionx_data = {
 	.kgsl_skip_zeroing = 0,
 	.avoid_dirty_pte = 0,
+	.bg_blocklist = "com.shopee.id,com.lazada.android,com.tokopedia.tkpd",
 };
+
+bool fusionx_comm_blocked(const char *comm)
+{
+	int i;
+
+	for (i = 0; i < fusionx_blocked_cnt; i++) {
+		if (!strncmp(comm, fusionx_blocked[i], fusionx_blocked_len[i]))
+			return true;
+	}
+
+	return false;
+}
+EXPORT_SYMBOL_GPL(fusionx_comm_blocked);
+
+static void fusionx_rebuild_blocklist(char *buf)
+{
+	char *p = buf;
+	char *token;
+
+	fusionx_blocked_cnt = 0;
+	while ((token = strsep(&p, ",")) &&
+	       fusionx_blocked_cnt < FUSIONX_MAX_BLOCKED) {
+		if (!*token)
+			continue;
+
+		strscpy(fusionx_blocked[fusionx_blocked_cnt], token, TASK_COMM_LEN);
+
+		fusionx_blocked_len[fusionx_blocked_cnt] =
+			strlen(fusionx_blocked[fusionx_blocked_cnt]);
+
+		pr_alert("FUSIONX: blocking '%s'\n",
+			 fusionx_blocked[FUSIONX_blocked_cnt]);
+		fusionx_blocked_cnt++;
+	}
+	pr_alert("FUSIONX: total blocked apps = %d\n", fusionx_blocked_cnt);
+}
+
+static ssize_t bg_blocklist_show(struct kobject *kobj,
+				 struct kobj_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%s\n", fusionx_data.bg_blocklist);
+}
+
+static ssize_t bg_blocklist_store(struct kobject *kobj,
+				  struct kobj_attribute *attr, const char *buf,
+				  size_t count)
+{
+	char tmp[FUSIONX_BLOCKLIST_STRLEN];
+
+	strscpy(tmp, buf, sizeof(tmp));
+	strreplace(tmp, '\n', '\0');
+	strscpy(fusionx_data.bg_blocklist, tmp, sizeof(fusionx_data.bg_blocklist));
+
+	fusionx_rebuild_blocklist(tmp);
+
+	return count;
+}
+
+static struct kobj_attribute bg_blocklist_attr =
+	__ATTR(bg_blocklist, 0664, bg_blocklist_show, bg_blocklist_store);
 
 #define FUSIONX_ATTR_RW(name)                                                     \
 	static ssize_t name##_show(struct kobject *kobj,                       \
@@ -33,7 +98,8 @@ FUSIONX_ATTR_RW(kgsl_skip_zeroing);
 FUSIONX_ATTR_RW(avoid_dirty_pte);
 
 static struct attribute *fusionx_attrs[] = { &kgsl_skip_zeroing_attr.attr,
-					  &avoid_dirty_pte_attr.attr, NULL };
+					  &avoid_dirty_pte_attr.attr,
+					  &bg_blocklist_attr.attr, NULL };
 
 static struct attribute_group fusionx_attr_group = {
 	.attrs = fusionx_attrs,
@@ -44,6 +110,12 @@ static struct kobject *fusionx_kobj;
 static int __init fusionx_attributes_init(void)
 {
 	int retval;
+	char tmp[FUSIONX_BLOCKLIST_STRLEN];
+
+	if (fusionx_data.bg_blocklist[0]) {
+		strscpy(tmp, fusionx_data.bg_blocklist, sizeof(tmp));
+		fusionx_rebuild_blocklist(tmp);
+	}
 
 	fusionx_kobj = kobject_create_and_add("fusionx_attributes", kernel_kobj);
 	if (!fusionx_kobj)
